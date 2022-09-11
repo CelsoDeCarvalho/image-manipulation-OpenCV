f6a1121b243778ca88df904ab2f43e252c58d2f9
Celso Octaviano De Carvalho <celso@celso>
2022-09-11 02:25:19 +0200

    chore: added open cv maven library

--- src/main/java/dev/mozcoder/cg/opencv/Program.java
+++ src/main/java/dev/mozcoder/cg/opencv/Program.java
@@ -1,7 +1,8 @@
 package dev.mozcoder.cg.opencv;
-
 public class Program{
     public static void main(String[] args) {
         System.out.println("Ola. Comecando com open cv");
+        nu.pattern.OpenCV.loadLibrary();
+        System.out.println("Open cv loaded successfully");
     }
 }
